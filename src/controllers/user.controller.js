750c1025bd051b086b8969d70fc7097cbf669d4b
Preetam Shri <110012624+pshri5@users.noreply.github.com>
2025-01-23 23:05:17 +0530

    update user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -37,7 +37,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
     const {fullName, email, username, password } = req.body
-    //console.log("email: ", email);
+    
 
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -52,7 +52,7 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-    //console.log(req.files);
+    
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
